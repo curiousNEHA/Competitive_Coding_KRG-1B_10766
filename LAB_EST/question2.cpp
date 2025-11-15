8018733fa5152eb2581c8cebb82f66a256af63fa
Neha Sharma <155069649+curiousNEHA@users.noreply.github.com>
2025-11-15 11:48:10 +0530

    Implement uniquePathsWithObstacles function

--- LAB_EST/question2.cpp
+++ LAB_EST/question2.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    int func(int i,int j,vector<vector<int>>& dp,vector<vector<int>>& obstacleGrid){
+        if(i>=0 && j>=0 && obstacleGrid[i][j]==1) return 0;
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int up = func(i-1,j,dp,obstacleGrid);
+        int left = func(i,j-1,dp,obstacleGrid);
+        return dp[i][j] = up+left;
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int n = obstacleGrid.size();
+        int m = obstacleGrid[0].size();
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        int result = func(n-1,m-1,dp,obstacleGrid);
+          return result;
+    }
+};
